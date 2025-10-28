7e207133512fc682948b6fcf6fd8fb91e2e1095e
Samir Bhattarai <135333951+mr-samirbhattarai@users.noreply.github.com>
2025-10-28 17:00:16 +0000

    Add routing and deployment configuration for React app
    - Added a star.svg icon to the build directory.
    - Updated package-lock.json to include gh-pages for deployment.
    - Modified package.json to set homepage, add predeploy and deploy scripts.
    - Created a 404.html file for redirecting to the homepage.
    - Implemented HashRouter with Routes in App.jsx for navigation.
    - Configured Vite to set base path for GitHub Pages and changed output directory to 'build'.

--- my-first-react-app/vite.config.js
+++ my-first-react-app/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  base: '/react-practice/',
+  build: {
+    outDir: "build", // change from default 'dist'
+  },
+});
